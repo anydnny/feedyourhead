5528217b5356081c1f271f327ad42fc33036bc9f
Daniil Glushakov <70380061+anydnny@users.noreply.github.com>
2022-10-21 23:22:27 +0500

    move branch to dev

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -38304,7 +38304,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50817" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51875" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
